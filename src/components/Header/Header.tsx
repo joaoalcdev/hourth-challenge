804fc66f0ef799867ef4aecb3426ea2e16d8094e
João Alcântara <joaosantalcantara@gmail.com>
2022-02-10 20:42:15 -0300

    fix font-size mobile  hero section

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -13,7 +13,7 @@ export function Header() {
           <div className="flex items-center justify-start pt-0 pl-6">
             <div className="px-3 py-6">
               <div className="lg:2/6 xl:w-2/4 mt-20 lg:mt-40 lg:ml-16 text-left">
-                <div className="text-6xl font-semibold text-gray-900 leading-none text-6xl">
+                <div className="text-6xl font-semibold text-gray-900 leading-none sm:text-2xl text-6xl">
                   Tenha até 3x mais lucro em sua operação de dropshipping. Processando pedidos fora do aliexpress.
                 </div>
                 <div className="mt-6 text-xl font-light text-true-gray-500 antialiased">
