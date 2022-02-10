bd211a241a173bf18076e9f1135b7fe59c0f7ba3
João Alcântara <joaosantalcantara@gmail.com>
2022-02-10 15:31:52 -0300

    finished installation

---
+++ global/styles/theme.ts
@@ -0,0 +1,28 @@
+export const theme = {
+  colors: {
+    primary: '#000000',
+
+    secondary100: '#000000',
+    secondary90: '#000000',
+    secondary80: '#000000',
+    secondary70: '#000000',
+    secondary60: '#000000',
+    secondary50: '#000000',
+    secondary40: '#000000',
+    secondary30: '#000000',
+
+    overlay: 'rgba(0, 0, 0, 0.7)',
+    hightlight: '#000000',
+    heading: '#000000',
+    line: '#000000',
+    on: '#000000',
+
+    // backgroundColor: '#000000',
+  },
+  fonts: {
+    title000: '',
+    title000: '',
+    text000: '',
+    text000: '',
+  }
+}
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+
+
+export function Navbar() {
+  return (
+    <section className="">
+      <h1>test</h1>
+    </section>
+  )
+}
